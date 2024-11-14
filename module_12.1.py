f7b50609523c1b69ea528ddf4725d03473c3bbd5
Safar <safqwer.136@gmail.com>
2024-11-15 00:12:10 +0300

    Add files via upload

--- module_12.1.py
+++ module_12.1.py
@@ -20,28 +20,22 @@ class RunnerTest(unittest.TestCase):
 
     def test_walk(self):
         begun1 = Runner('misha')
-        i = 0
-        while i < 10:
+        for _ in range(10):
             begun1.walk()
-            i += 1
         self.assertEqual(begun1.distance, 50)
 
     def test_run(self):
         begun2 = Runner('sasha')
-        i = 0
-        while i < 10:
+        for _ in range(10):
             begun2.run()
-            i += 1
         self.assertEqual(begun2.distance, 100)
 
     def test_challenge(self):
         begun3 = Runner('denis')
         begun4 = Runner('max')
-        i = 0
-        while i < 10:
+        for _ in range(10):
             begun3.run()
             begun4.walk()
-            i += 1
         self.assertNotEqual(begun3.distance, begun4.distance)
 
 
